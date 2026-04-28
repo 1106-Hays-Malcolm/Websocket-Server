2d52c8cb5648933433d81455fb033e5470f43145
Malcolm Hays <mah2005atM@gmail.com>
2026-04-28 12:24:22 -0700

    Send a message to the socket when button pressed

--- HTML/websocket_client.js
+++ HTML/websocket_client.js
@@ -1,11 +1,16 @@
 console.log("Connecting!");
 const socket = new WebSocket('ws://localhost:8080');
 
-// function Connect()
-// {
-//     console.log("Connecting!");
-// }
+function Connect()
+{
+    socket.send(`Button pressed!`);
+}
 
 socket.onopen = function(event) {
     console.log("Connected!");
+    socket.send(`Hi there!`);
+};
+
+socket.onclose = function(event) {
+    console.log("Closed!");
 };
