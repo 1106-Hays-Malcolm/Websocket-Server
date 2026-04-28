321c65f925836d400149b3be4edadb7978298a5a
Malcolm Hays <mah2005atM@gmail.com>
2026-04-28 11:01:25 -0700

    Print message when client connects to server

--- HTML/websocket_client.js
+++ HTML/websocket_client.js
@@ -1,5 +1,11 @@
-function Connect()
-{
-    console.log("Connecting!");
-    const socket = new WebSocket('ws://localhost:8080');
-}
+console.log("Connecting!");
+const socket = new WebSocket('ws://localhost:8080');
+
+// function Connect()
+// {
+//     console.log("Connecting!");
+// }
+
+socket.onopen = function(event) {
+    console.log("Connected!");
+};
